1c1d2035f64946c44a52f1a72522d128955a405f
jvogit <justinxvopro@gmail.com>
2021-01-19 04:02:10 -0800

    add mobilenavbar

---
+++ src/components/navbar/MobileNavbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { HeaderNavigation, ALIGN, StyledNavigationList, StyledNavigationItem } from 'baseui/header-navigation';
+import { Button, KIND, SIZE } from 'baseui/button';
+import { Menu } from "baseui/icon";
+import { Drawer } from 'baseui/drawer';
+import { StatefulMenu } from 'baseui/menu';
+
+const MobileNavbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <HeaderNavigation
+      style={{
+        padding: "10px",
+      }}
+    >
+      <StyledNavigationList $align={ALIGN.left}>
+        <StyledNavigationItem>Rescue Groups API Explorer</StyledNavigationItem>
+      </StyledNavigationList>
+      <StyledNavigationList $align={ALIGN.center} />
+      <StyledNavigationList $align={ALIGN.right}>
+        <StyledNavigationItem>
+          <Button
+            kind={KIND.minimal}
+            size={SIZE.mini}
+            onClick={() => setOpen(true)}
+          >
+            <Menu size={32} />
+          </Button>
+
+          <Drawer
+            isOpen={open}
+            renderAll
+            onClose={() => setOpen(false)}
+          >
+            <StatefulMenu
+              items={[
+                { label: "Documentation", callback: () => { window.open("https://api.rescuegroups.org/v5/public/docs", "_blank") } }
+              ]}
+              onItemSelect={({ item }) => { item.callback(); setOpen(false); }}
+              renderAll
+            />
+          </Drawer>
+        </StyledNavigationItem>
+      </StyledNavigationList>
+    </HeaderNavigation>
+  );
+};
+
+export default MobileNavbar;
